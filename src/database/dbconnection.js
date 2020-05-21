3b2e445e6988008ff44c9ef785bd5b8f6e48c5fa
James Daniels <jrd656@gmail.com>
2020-05-21 10:22:57 +0300

    Moris comments address

--- src/database/dbconnection.js
+++ src/database/dbconnection.js
@@ -1,13 +1,13 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 let DATABASE_URL = process.env.DATABASE_URL
+
+if (process.env.NODE_ENV !== 'production') {
+    DATABASE_URL = process.env.TEST_DB_URL
+}
  
 if (!process.env.DATABASE_URL) {
     throw new Error('DB_URL must be set')
-}
- 
-if (process.env.NODE_ENV !== 'production') {
-    DATABASE_URL = process.env.TEST_DB_URL
 }
 
 module.exports = new Pool({
