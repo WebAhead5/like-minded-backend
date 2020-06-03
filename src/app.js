c6165e72b2daee64f8f29712f8ef14e73d783707
hadikalil <hadikalilweb@gmail.com>
2020-06-03 16:32:58 +0300

    ampliment user settings sql+model+route

--- src/app.js
+++ src/app.js
@@ -5,14 +5,17 @@ const mainRouter = require('./routers/main/router')
 const usersRouter = require('./routers/userProfile/router')
 const relationshipRouter = require('./routers/relationships/router')
 const messagesRouter = require('./routers/messages/router')
+const settingsRouter = require('./routers/settings/router')
 
 app.use(express.json())
 
 
 app.use(usersRouter)
 app.use(relationshipRouter)
-app.use(mainRouter)
 app.use(messagesRouter)
+app.use(settingsRouter)
+app.use(mainRouter)
+
 //app.get('/', (req, res) => res.sendFile(path.join(__dirname, "/index.html")))
 
 
--- src/model/userProfile.model.js
+++ src/model/userProfile.model.js
@@ -19,17 +19,19 @@ exports.update = async (userId, fields) => {
     if (!fields) throw Error("No fields provided");
     if (isNaN(userId)) throw Error("userId is not a number");
 
-    let keys = Object.keys(fields)
-    let columnNames = [ "firstname", "lastname", "gender", "status", "bio", "job", "livingin", "primaryphoto"]
-    
-    if (keys.some(key =>!columnNames.includes(key.toLowerCase())
+    let keys = Object.keys(fields);
+    let values = Object.values(fields);
+    let columnNames = ["firstname", "lastname", "gender", "status", "bio", "job", "livingin", "primaryphoto"]
+
+    if (keys.some(key => !columnNames.includes(key.toLowerCase())
     )) {
         throw Error("invalid field provided")
     }
-    try {
-            let sqlCommand = `update userProfile set ${keys.map((key,index)=>`${key} = $${index+2}`).join(" , ")} where userid = $1  `;
-            await db.query(sqlCommand, [userId, ...keys ])
 
+    try {
+        let sqlCommand = `update userProfile set ${keys.map((key, index) => `${key} = $${index + 2}`).join(" , ")} where userid = $1  `;
+        await db.query(sqlCommand, [userId, ...values])
+        return { message: "userProfile updated successfully" }
     } catch (error) {
         throw error
     }
@@ -39,9 +41,37 @@ exports.update = async (userId, fields) => {
 // Delete user profile with userId
 exports.delete = async (userId) => {
     await db.query(`delete from userProfile where userId = $1`, [userId])
+    return { message: "userProfile deleted successfully" }
 }
 
 // Add user with userId and form information.
-exports.add = async ({ userId, firstName, lastName, gender }) => {
-    await db.query(`insert into userprofile(userid, firstname, lastname, gender) values ($1,$2,$3,$4)`, [userId, firstName, lastName, gender])
+exports.add = async (fields) => {
+    if (!fields) throw Error("No fields provided");
+    //if (isNaN(userId)) throw Error("userId is not a number");
+    let columnNames = ["firstname", "lastname", "gender", "status", "bio", "job", "livingin", "primaryphoto"];
+    let columnNamesVital = ["firstname", "lastname", "gender"];
+
+    let keys = Object.keys(fields).map(key => key.toLowerCase());
+    let values = Object.values(fields);
+
+    if (!columnNamesVital.every(vitalColumn => keys.includes(vitalColumn))) {
+        throw Error("invalid or no vital field provided")
+    }
+
+    if (keys.some(key => !columnNames.includes(key))) {
+        throw Error("invalid field provided")
+    }
+
+    try {
+        let sqlCommand = `INSERT into userProfile ( ${keys.map((key) => `${key}`).join(" , ")} ) values ( ${keys.map((key, index) => `$${index + 1}`).join(" , ")} )`;
+        await db.query(sqlCommand, [...values])
+        return { message: "userProfile created successfully" }
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+
+
 }
+
+// insert into userprofile(userId, firstName, lastName , gender, status, bio, primaryPhoto) values ($1,$2,$3,$4,$5,$6,$7)
