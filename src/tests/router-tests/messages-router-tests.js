a0c1272a6ba91b059429bd15733104088b370bf9
hadikalil <hadikalilweb@gmail.com>
2020-06-05 17:18:23 +0300

    not much from the last commit

--- src/tests/router-tests/messages-router-tests.js
+++ src/tests/router-tests/messages-router-tests.js
@@ -40,13 +40,13 @@ test("route to get all relationships where user and candidates like each other",
 
 test('get all messages when userId=1 and otherUserId=2', t => {
     supertest(router)
-        .get("/messages")
+        .get("/messages/2")
         .send({userId: 1})
         .expect(200)
         .expect("content-type", "application/json; charset=utf-8")
         .end((err, res) => {
-            let actual = res.body.data
-            let expected = [ { profile: { id: 2, userid: 2, firstname: 'Moris', lastname: 'Rafoul', gender: 'male', status: 'Single', bio: 'Teaching web-dev', job: 'Web-Developer', livingin: 'Haifa', primaryphoto: 'https://avatars0.githubusercontent.com/u/10247681?s=60&v=4', subphotos: [ 'https://avatars2.githubusercontent.com/u/51966598?s=60&v=4', 'https://avatars0.githubusercontent.com/u/10247681?s=60&v=4', 'https://avatars0.githubusercontent.com/u/10247681?s=60&v=4' ] } } ]
+            let actual = res.body
+            let expected = []
             t.deepEquals(actual, expected)
             t.error(err);
             t.end();
