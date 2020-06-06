dad9194275b65daee04b9f3446d20ded810ff44c
moris <moris.rafol@gmail.com>
2020-06-07 02:09:38 +0300

    fix: add google auth router.js

--- src/routers/auth/router.js
+++ src/routers/auth/router.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = express.Router();
+
+
 const login = require("./routes/login")
 const logout = require("./routes/logout")
 const currentUser  =require("./routes/currentUser");
 const register  =require("./routes/register");
 const deleteAccount  =require("./routes/deleteAccount");
+
 
 router.post("/login",login.post);
 router.all("/logout",logout.get);
---
+++ src/routers/googleAuth/router.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const router = express.Router();
+const envCheck = require("../../tools/envCheck");
+
+const passport =  require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+
+const googleClientID = envCheck.inProduction? process.env.GOOGLE_CLIENT_ID: process.env.DEV_GOOGLE_CLIENT_ID;
+const googleClientSecret = envCheck.inProduction? process.env.GOOGLE_SECRET: process.env.DEV_GOOGLE_SECRET;
+
+
+passport.use(new GoogleStrategy({
+    clientID: googleClientID,
+    clientSecret: googleClientSecret,
+    callbackURL: "/auth/google/callback"
+},
+(accessToken,refreshToken, profile,done)=>{
+
+    console.log(profile)
+    done()
+
+}));
+
+router.get("/google",
+    passport.authenticate("google",{
+        scope: ["profile","email"]
+    })
+
+)
+
+router.get("/google/callback",
+    passport.authenticate("google")
+)
+
+
+module.exports = router
