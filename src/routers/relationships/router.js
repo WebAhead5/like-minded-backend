1c80a176a119f422b5c9e323c803a9be4e0d2fdc
moris <moris.rafol@gmail.com>
2020-06-09 00:46:06 +0300

    move routes to app.js

--- src/routers/relationships/router.js
+++ src/routers/relationships/router.js
@@ -3,63 +3,15 @@ const serverResponse = require("../../tools/serverResponse")
 
 const matches = require('./routes/matches')
 const relationship = require('./routes/relationship')
-const relationshipStatuses = require('./routes/relationshipStatuses')
 const relationshipsModel = require('../../model/relationships.model')
-const quizzesModel = require('../../model/quizzes.model')
-
-///////////////////// QUIZZES /////////////////////////////////
-
-router.get('/quizzes', async (req,res)=>{
-    let { userId } = req.userId || req.body
-    try {
-        let quizzesData = await quizzesModel.getQuizzesData(userId)
-        serverResponse.sendData(res, { data: quizzesData })
-    } catch (error) {
-        serverResponse.sendError(res, {message: error.message })
-    }
-})
-
-router.post('/quizzes/question/:questionId', async (req,res)=>{
-    let { questionId } = req.params;
-    let { userId } = req.userId || req.body;
-    let {userAnswer} = req.body;
-    try {
-        let quizQuestionPosted = await quizzesModel.setAnswer(userId, questionId, userAnswer)
-        serverResponse.sendData(res, { data: quizQuestionPosted })
-    } catch (error) {
-        serverResponse.sendError(res, {message: error.message })
-    }
-})
-
-router.get('/quizzesResults/:quizTitle', async (req,res)=>{
-    let { quizTitle } = req.params;
-    let { userId } = req.userId || req.body   
-    try {
-        let quizResults = await quizzesModel.getQuizResult(userId, quizTitle)
-        serverResponse.sendData(res, { data: quizResults })
-    } catch (error) {
-        serverResponse.sendError(res, {message: error.message })
-    }
-})
-
-router.get('/quizzesMatch', async (req,res)=>{
-    let { userId } = req.userId || req.body  
-     let {candidateId, quizType} = req.body;
-    try {
-        let quizResults = await quizzesModel.getQuizzesMatchResults(userId, candidateId, quizType)
-        serverResponse.sendData(res, { data: quizResults })
-    } catch (error) {
-        serverResponse.sendError(res, {message: error.message })
-    }
-})
 
 /////////////////////////////// RELATIONSHIPS ////////////////////////////////////
 
 // Get all relationships where user and candidates both like each other
-router.get('/relationship/matches', matches.get)
+router.get('/matches', matches.get)
 
 // Get all relationships where user and candidates both like each other
-router.get('/relationship/othersSelection', async (req,res)=>{
+router.get('/othersSelection', async (req,res)=>{
     let { userId } = res
     try {
         let candidatesThatAreSelected = await relationshipsModel.getRelationshipsWhereCandidateSelected(null, userId);
@@ -69,7 +21,7 @@ router.get('/relationship/othersSelection', async (req,res)=>{
     }
 })
 
-router.get('/relationship/othersSelection/:status', async (req,res)=>{
+router.get('/othersSelection/:status', async (req,res)=>{
     let { status } = req.params
     let { userId } = res;
     try {
@@ -80,7 +32,7 @@ router.get('/relationship/othersSelection/:status', async (req,res)=>{
     }
 })
 
-router.get('/relationship/userSelection', async (req,res)=>{
+router.get('/userSelection', async (req,res)=>{
     let { userId } = res
     try {
         let candidatesThatAreSelected = await relationshipsModel.getRelationshipWhereUserSelected(null, userId);
@@ -90,7 +42,7 @@ router.get('/relationship/userSelection', async (req,res)=>{
     }
 })
 
-router.get('/relationship/userSelection/:status', async (req,res)=>{
+router.get('/userSelection/:status', async (req,res)=>{
     let { status } = req.params
     let { userId } = res
     try {
@@ -102,12 +54,12 @@ router.get('/relationship/userSelection/:status', async (req,res)=>{
 })
 
 // Get and set relationship status between user and specified candidate
-router.route('/relationship/:candidateId')
+router.route('/:candidateId')
     .get(relationship.get)
     .post(relationship.post);
 
 
-router.route('/relationship/:candidateId/:status')
+router.route('/:candidateId/:status')
     .post(relationship.postRoute);
 
 module.exports = router;
