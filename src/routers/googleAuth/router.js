9b7f8d51bf3d7be8d710eba9791fea960251d34b
moris <moris.rafol@gmail.com>
2020-06-09 22:22:59 +0300

    feat: try something

--- src/routers/googleAuth/router.js
+++ src/routers/googleAuth/router.js
@@ -25,8 +25,20 @@ passport.use(new GoogleStrategy({
 done(null, profile)
 
 }));
-//
 
+const cors = require('cors');
+
+app.use('*', function(req, res, next) {
+//replace localhost:8080 to the ip address:port of your server
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Credentials', true);
+    next();
+});
+
+//enable pre-flight
+app.options('*', cors());
 
 
 router.get("/",blockedFromLoggedInUsers,
@@ -47,7 +59,7 @@ router.get("/callback",
                     .then(( {expires, sessionId})=>{
 
                         res.cookie("sid", sessionId, {expires: expires,signed:true})
-                        res.redirect("http://localhost:3000/auth/google/successful")
+                        res.redirect("/auth/google/successful")
 
                     }).catch(e=>res.redirect("/auth/google/unsuccessful"))
 
