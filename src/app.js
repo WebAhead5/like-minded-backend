f888a8caa100e1b507f74900a83063b08ee9f035
moris <moris.rafol@gmail.com>
2020-06-10 08:58:56 +0300

    feat: try something

--- src/app.js
+++ src/app.js
@@ -34,7 +34,7 @@ app.use((req,res,next)=>{
 if(!process.env.COOKIE_SECRET)
     throw new Error("cookie secret must be provided");
 app.use(cookieParser(process.env.COOKIE_SECRET))
-app.use(express.json())
+app.use(express.json({limit:'50mb' }))
 app.use(helmet())
 app.use(favicon(path.join(__dirname,"..","public","favicon.ico")))
 app.use(loadLoggedInUserId)
