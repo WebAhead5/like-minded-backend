b3c17c7420c4eb1ec10fe8f28870c98eca0acfe3
moris <moris.rafol@gmail.com>
2020-06-12 00:59:06 +0300

    fix returned value

--- src/routers/auth/router.js
+++ src/routers/auth/router.js
@@ -8,6 +8,7 @@ const logout = require("./routes/logout")
 const currentUser  =require("./routes/currentUser");
 const register  =require("./routes/register");
 const deleteAccount  =require("./routes/deleteAccount");
+const isLoggedIn  =require("./routes/isLoggedIn");
 
 
 router.post("/login",blockedFromLoggedInUsers,login.post);
@@ -15,7 +16,7 @@ router.all("/logout",requireUserToLogin,logout.get);
 router.post("/register",blockedFromLoggedInUsers,register.post);
 router.get("/currentUser",requireUserToLogin,currentUser.get);
 router.post("/deleteAccount",requireUserToLogin,deleteAccount.get);
-
+router.post("/isLoggedIn.js", isLoggedIn);
 
 
 module.exports = router
