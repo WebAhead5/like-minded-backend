634aa247b3b8eb0356d2a8c5dc44ebf8fdf7c1f7
moris <moris.rafol@gmail.com>
2020-06-14 02:03:17 +0300

    FIX: AGAIN FIX MATCHES RESULT

--- src/model/matches.model.js
+++ src/model/matches.model.js
@@ -20,40 +20,39 @@ exports.getPotentialMatches = async (userId, {count, offset}) => {
     }
 
     let potentialMatches = await db.query(`
-        select * 
-        from userprofile
-        where userid in
-              (
-                 
-                 (
-                  -- get all users id's that the logged in user was liked by 
-                  SELECT userid from userprofile
-              
-                         inner join userRelationship ON userRelationship."userId1" = userprofile.userId OR
-                                                        userRelationship."userId2" = userprofile.userId
-                WHERE ($1 in (userRelationship."userId1", userRelationship."userId2"))
-                  and (userRelationship."user2-towards-user1" != userRelationship."user1-towards-user2")
-                  and (userRelationship."userId1" = userid and userRelationship."user1-towards-user2" not in('like','block')
-                      or userRelationship."userId2" = userid and userRelationship."user2-towards-user1" not in('like','block') )
-                  and userid != $1
-                  )
-             
-               union distinct
-             
-               -- all the people that the logged in user doesn't have a relationship with
-             (
-                 select userid from userprofile where userid not in
-                       ( SELECT distinct userid from userprofile
-                    
-                                               inner join userRelationship ON userRelationship."userId1" = userprofile.userId OR
-                                                                              userRelationship."userId2" = userprofile.userId
-                        WHERE $1 in (userRelationship."userId1", userRelationship."userId2")
-                        )
 
-             )
+    select * from userprofile
+    where userid in
+      (
+
+          (
+              -- get all users id's that the logged in user was liked by
+              SELECT distinct userid from userprofile
+              inner join userRelationship ON userRelationship."userId1" = userprofile.userId OR userRelationship."userId2" = userprofile.userId
+              WHERE ($1 in (userRelationship."userId1", userRelationship."userId2"))
+                and (userRelationship."userId1" = $1 and userRelationship."user1-towards-user2" = 'none'
+                  or userRelationship."userId2" = $1 and userRelationship."user2-towards-user1" = 'none' )
+                and userid != $1
+          )
+
+          union distinct
+
+          (
+              -- all the people that the logged in user doesn't have a relationship with
+              select userid from userprofile where userid not in
+                 ( 
+                     SELECT distinct userid from userprofile 
+                         nner join userRelationship ON userRelationship."userId1" = userprofile.userId OR                                                     
+                                                       userRelationship."userId2" = userprofile.userId                       
+                     WHERE $1 in (userRelationship."userId1", userRelationship."userId2")
+                                           
+                 )
+              
+          )
+  )
                   
                   
-  ) ${countStr} ${offsetStr} ; `, [userId])
+   ${countStr} ${offsetStr} ; `, [userId])
 
 
     let getQuizResults = async (userId, candidateId, quizType) => await quizzesModel.getQuizzesMatchResults(userId, candidateId, quizType)
