837f4e4f0f76251ac55e3768e44d0cccff5c190d
moris <moris.rafol@gmail.com>
2020-06-14 01:12:45 +0300

    fix:  matches return val

--- src/model/matches.model.js
+++ src/model/matches.model.js
@@ -8,13 +8,13 @@ exports.getPotentialMatches = async (userId, {count, offset}) => {
     await validators.checkUserExists(userId);
     let countStr = ""
     let offsetStr = ""
-    if(count!== undefined) {
-        if(Number.isNaN(count))
+    if (count !== undefined) {
+        if (Number.isNaN(count))
             throw new Error("count mist be a number")
         countStr = "limit " + count.toString();
     }
-    if(offset !== undefined) {
-        if(Number.isNaN(count))
+    if (offset !== undefined) {
+        if (Number.isNaN(count))
             throw new Error("count mist be a number")
         offsetStr = "offset " + count;
     }
@@ -23,16 +23,37 @@ exports.getPotentialMatches = async (userId, {count, offset}) => {
         select * 
         from userprofile
         where userid in
-              ((SELECT userid from userprofile
+              (
+                 
+                 (
+                  -- get all users id's that the logged in user was liked by 
+                  SELECT userid from userprofile
+              
                          inner join userRelationship ON userRelationship."userId1" = userprofile.userId OR
                                                         userRelationship."userId2" = userprofile.userId
                 WHERE ($1 in (userRelationship."userId1", userRelationship."userId2"))
                   and (userRelationship."user2-towards-user1" != userRelationship."user1-towards-user2")
-                  and userid != $1)
+                  and (userRelationship."userId1" = userid and userRelationship."user1-towards-user2" not in('like','block')
+                      or userRelationship."userId2" = userid and userRelationship."user2-towards-user1" not in('like','block') )
+                  and userid != $1
+                  )
              
                union distinct
-               ( SELECT userid from userprofile  left join userrelationship on null where userid != $1))
-   ${countStr} ${offsetStr} ; `, [userId])
+             
+               -- all the people that the logged in user doesn't have a relationship with
+             (
+                 select userid from userprofile where userid not in
+                       ( SELECT distinct userid from userprofile
+                    
+                                               inner join userRelationship ON userRelationship."userId1" = userprofile.userId OR
+                                                                              userRelationship."userId2" = userprofile.userId
+                        WHERE $1 in (userRelationship."userId1", userRelationship."userId2")
+                        )
+
+             )
+                  
+                  
+  ) ${countStr} ${offsetStr} ; `, [userId])
 
 
     let getQuizResults = async (userId, candidateId, quizType) => await quizzesModel.getQuizzesMatchResults(userId, candidateId, quizType)
