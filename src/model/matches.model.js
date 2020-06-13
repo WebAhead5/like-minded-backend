e80ad88224ebf4f99c1fc0f03fa72c6f14662dcd
moris <moris.rafol@gmail.com>
2020-06-13 23:24:03 +0300

    feat: return all potential matches

--- src/model/matches.model.js
+++ src/model/matches.model.js
@@ -8,12 +8,20 @@ exports.getPotentialMatches = async (userId, options = {}) => {
     await validators.checkUserExists(userId);
 
     let potentialMatches = await db.query(`
-    SELECT * FROM userprofile
-    INNER JOIN userRelationship ON userRelationship."userId1" = userprofile.userId OR userRelationship."userId2" = userprofile.userId
-    WHERE (userRelationship."userId1" = $1 AND userRelationship."user1-towards-user2" = 'none' AND userprofile.userid != $1) OR (userRelationship."userId2" = $1 AND userRelationship."user2-towards-user1" = 'none' AND userprofile.userid != $1) ;
-    `    , [userId])
+        select * 
+        from userprofile
+        where userid in
+              ((SELECT userid from userprofile
+                         inner join userRelationship ON userRelationship."userId1" = userprofile.userId OR
+                                                        userRelationship."userId2" = userprofile.userId
+                WHERE ($1 in (userRelationship."userId1", userRelationship."userId2"))
+                  and (userRelationship."user2-towards-user1" != userRelationship."user1-towards-user2")
+                  and userid != $1)
+             
+               union distinct
+               ( SELECT userid from userprofile  left join userrelationship on null where userid != $1));;
+    `, [userId])
 
-    
 
     let getQuizResults = async (userId, candidateId, quizType) => await quizzesModel.getQuizzesMatchResults(userId, candidateId, quizType)
 
